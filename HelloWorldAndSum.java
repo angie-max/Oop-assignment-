b3e097b142726a7e3e75ba7b841a0d6356ce2b82
pauline-max <paulinewachira74@gmail.com>
2025-01-23 21:09:02 +0300

    Add files via upload

---
+++ HelloWorldAndSum.java
@@ -0,0 +1,18 @@
+// Admission Number: CT101/G/20337/23
+// Name: 
+// Description: This program prints hello world and adds 2 variables (5 and 7) to find the sum and display the results.
+public class HelloWorldAndSum {
+  
+    public static void main(String[] args) {
+        // part 1 - it Prints "Hello World" 
+        System.out.println("Hello World");
+
+        // Part 2 - declare and initialize the variables, plus adding them to get the sum
+        int x = 5; 
+        int y = 7; 
+        int sum = x + y; 
+      // Displaying the result of the sum
+        System.out.println("The sum of x and y is: " + sum);
+    }
+}
+
